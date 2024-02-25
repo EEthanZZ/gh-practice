4a8e89bcb261d78dd0cb3e28619d4c78c3ed7018
EEthanZZ <zycc2727@gmail.com>
2024-02-26 05:53:35 +1100

    upload content to s3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,6 +3,16 @@ const github = require('@actions/github');
 const exec = require("@actions/exec");
 
 function run() {
+    // Get some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // Upload files
+    const s3URI = `s3://${bucket}`
+
+    exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`);
+
     core.notice('Hello From my custome JS Action!')
 }
 
